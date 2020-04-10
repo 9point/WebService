f8108027829742cd75771c99af9f552aaceddc9d
Brendan McNamara <me@brendan9.com>
2020-04-10 11:23:47 -0700

    Creates travis configuration

---
+++ scripts/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+
+docker push 9point/webservice:${PACKAGE_VERSION}
+
